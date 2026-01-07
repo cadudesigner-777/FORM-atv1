a1934964b1956c7d40df90a3a1e9be3866fc4236
cadudesigner-777 <cadudesigner2008@gmail.com>
2026-01-07 11:39:38 -0300

    Primeira atividade

---
+++ GIT-atv1/index.php
@@ -0,0 +1,30 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" class="form-nome">
+        <label for="nome">Nome:</label><br>
+        <input type="text" id="nome" name="nome" placeholder= "Didite seu nome" >
+        <br>
+        <br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
+
+
+<?php if(isset($_POST['nome']) && trim($_POST['nome']) !== ''){
+    $nome = trim($_POST['nome']);
+    echo "Seja bem-vindo, $nome!";
+} else{
+    echo "Por favor, insira seu nome";
+}
+?>
