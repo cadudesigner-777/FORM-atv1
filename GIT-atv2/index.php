cf94a28dc34556b3da4cbd6b4746a7b39e3c00b4
cadudesigner-777 <cadudesigner2008@gmail.com>
2026-01-09 08:55:13 -0300

    Mudando nome do arquivo resultado.php

--- GIT-atv2/index.php
+++ GIT-atv2/index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form method="post" class="form-nome">
+    <form action="resultado.php" method="post" class="form-nome">
         <label for="nome">Nome:</label><br>
         <input type="text" id="nome" name="nome" placeholder= "Didite seu nome" >
         <br>
@@ -20,30 +20,3 @@
     </form>
 </body>
 </html>
-
-
-<?php
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-$nome = $_POST['nome'];
-$idade = $_POST['idade'];
-$nota = $_POST['nota'];
-
-
-if($nota  > 7){
-    $situacao = "Você foi aprovado!";
-} elseif($nota >= 5){
-    $situacao = "Você está de recuperação";
-} else{
-    $situacao = "Reprovado";
-}
-
-echo "<h3>Resultado</h3> <br>";
-
-echo "Nome: $nome <br>";
-echo "Idade: $idade <br>";
-echo "Nota: $nota <br>";
-echo "Situação: $situacao <br>";
-
-}
-
-?>
